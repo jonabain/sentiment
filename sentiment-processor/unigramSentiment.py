7e21e6ad0bdeef60d86ea2b82c9f7870696178dd
Jonathan Bain <jonabain@umich.edu>
2015-05-19 10:54:06 -0400

    Update unigramSentiment.py

--- sentiment-processor/unigramSentiment.py
+++ sentiment-processor/unigramSentiment.py
@@ -3,23 +3,9 @@ from sentimentModel import *
 class UnigramSentiment(SentimentModel):
 
     def __init__(self):
-        """
-        This is the constructor.
-        """
         super(UnigramSentiment, self).__init__()
         
     def trainSentiment(self, sentiments):
-        """
-        Requires: sentiments are the positive and negative sentiments 
-            to train on, a list of full-sentence strings beginning with 
-            0 or 1
-        Modifies: self.sentimentD[zero] and self.sentimentD[one],
-            which are themselves 1D dictionaries with '0' or '1' as 
-            the first keys that ultimately contain word: frequency 
-            pairs. This serves to count frequencies of positive- and 
-            negative- sentiment words.
-        Effects: nothing
-        """
         zero = '0'
         one = '1'
         
@@ -41,14 +27,6 @@ class UnigramSentiment(SentimentModel):
                         self.sentimentD[one][word] += 1
         
     def readSentiment(self, song):
-        """
-        Requires: song is a song that already exists or 
-        was generated and loaded into a file.
-        Modifies: sentimentStr, which then has the probability
-        of the song having positive or negative sentiment.
-        Effects: returns the sentiment that has been read from
-        the inputted song.
-        """
         downL = DataLoader()
         downL.loadMusicSentiment(song)
         prob = self.probabilities(downL.sentData)
@@ -56,14 +34,6 @@ class UnigramSentiment(SentimentModel):
         return prob
         
     def probabilities(self, text):
-        """
-        Requires: Nothing
-        Modifies: self.probabilitysD, a 2D dictionary whose innermost
-            dictionaries that are words with the keys '0' and '1' 
-            and values that are probabilities. 
-        Effects: returns a sentiment string of positive or negative
-            probabilities
-        """
         zero = '0'
         one = '1'
 
